7c9f4f51839a190f83b9e5c63bf31ae63465b0f2
srmarshall0 <srmarshall@pbs.org>
2025-03-29 23:44:55 -0400

    some basic nav

---
+++ calendar-app/src/routes/availability/+page.svelte
@@ -0,0 +1,3 @@
+<main>
+	Page where folks can select themselves, and set their availaibility for the upcomming month.
+</main>
---
+++ calendar-app/src/routes/subs/+page.svelte
@@ -0,0 +1,1 @@
+<main>Space for people to request subs and see who might be avaiable?</main>
